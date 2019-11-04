2e108914213cd019189463f550539c4c26651457
Imad Youssoufi <imadyoussoufi@gmail.com>
2019-11-04 13:32:40 +0100

    Updates

---
+++ precache-manifest.cca584c9261f3bba34b9b7c6e6a788f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac687db539df5b23bedaffb96a68a176",
+    "url": "/bio/index.html"
+  },
+  {
+    "revision": "c93ce616350fb3f69f22",
+    "url": "/bio/static/css/main.2c050b19.chunk.css"
+  },
+  {
+    "revision": "ca76898dd2f4422ea1be",
+    "url": "/bio/static/js/2.6dad6076.chunk.js"
+  },
+  {
+    "revision": "c93ce616350fb3f69f22",
+    "url": "/bio/static/js/main.3c504917.chunk.js"
+  },
+  {
+    "revision": "e4e96e244cef190a7909",
+    "url": "/bio/static/js/runtime-main.bef51046.js"
+  },
+  {
+    "revision": "9c88fcd446a944499e32700e4ace4076",
+    "url": "/bio/static/media/bg-head1.9c88fcd4.jpg"
+  },
+  {
+    "revision": "3365abba6477c48e6be892cf6faa8f25",
+    "url": "/bio/static/media/logo-green-2x.3365abba.png"
+  }
+]);

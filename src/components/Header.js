353fd14c52a57097c83c661ddb85ba3abdc6aa27
2imad <imadyoussoufi@gmail.com>
2019-11-11 12:33:36 +0100

    add bio bg

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,18 @@
 import React from 'react'
-import headerLogo from '../assets/images/logo-green-2x.png'
+import downArrow from '../assets/images/downarrow.png'
 
 const Header = () => {
    return (
       <header className="header">
-         <div className="header__logo-box">
-            <img className="header__logo" src={headerLogo} alt="Logo" />
-         </div>
-         <div className="header__text-box">
-            <h1 className="heading-primary">
-               <span className="heading-primary--main">solutions</span>
-               <span className="heading-primary--sub">web Design</span>
-            </h1>
-            <a href="#" className="btn btn--white btn--animated">Learn more!</a>
-         </div>
+         <section className="header__content">
+            <div className="header__text-box">
+               <h1 className="heading-primary">
+                  <span className="animated-pop heading-primary--main">solutions</span>
+                  <span className="animated-pop heading-primary--sub">web Design</span>
+               </h1>
+               <a href="#" className="animated-pop btn">Learn more!</a>
+            </div>
+         </section>
       </header>
    )
 }
